ecc91b3daa8afdb4e74bc0ddb325b16dfe7ab241
Juan David Pardo <jhondeivid12@hotmail.com>
2022-03-27 21:16:44 -0500

    Creación activity 2 la cual contiene el formulario que el usuario debe diligenciar para iniciar el proceso de adopción de una mascota

--- app/src/main/java/com/example/appindividual/MainActivity2.java
+++ app/src/main/java/com/example/appindividual/MainActivity2.java
@@ -6,12 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
 
     private Button btn_Enviar;
-    private EditText pt_Nombres;
+    private EditText tb_Nombres;
+    private EditText tb_CorreoElectronico;
+    private EditText tb_Telefono;
+    private CheckBox cb_TieneMascotas;
+    private EditText tb_CantidadMascotas;
+    private EditText tb_AnimalAdoptar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +25,24 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         btn_Enviar = findViewById(R.id.btn_Enviar);
-        pt_Nombres = findViewById(R.id.pt_Nombre);
+        tb_Nombres = findViewById(R.id.tb_Nombres);
+        tb_CorreoElectronico = findViewById(R.id.tb_CorreoElectronico);
+        tb_Telefono = findViewById(R.id.tb_Telefono);
+        cb_TieneMascotas = findViewById(R.id.cb_TieneMascotas);
+        tb_CantidadMascotas = findViewById(R.id.tb_CantidadMascotas);
+        tb_AnimalAdoptar = findViewById(R.id.tb_AnimalAdoptar);
 
         btn_Enviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Bundle bundle = new Bundle();
-                bundle.putString("Nombres", pt_Nombres.getText().toString());
+                bundle.putString("Nombres", tb_Nombres.getText().toString());
+                bundle.putString("CorreoElectronico", tb_CorreoElectronico.getText().toString());
+                bundle.putString("Telefono", tb_Telefono.getText().toString());
+                bundle.putString("TieneMascotas", cb_TieneMascotas.isChecked() ? "Si" : "No");
+                bundle.putString("CantidadMascotas", tb_CantidadMascotas.getText().toString());
+                bundle.putString("AnimalAdoptar", tb_AnimalAdoptar.getText().toString());
+
                 Intent i = new Intent(MainActivity2.this, MainActivity3.class);
                 i.putExtras(bundle);
                 startActivity(i);
